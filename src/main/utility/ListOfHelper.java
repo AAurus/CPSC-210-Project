c4ac374a6ebef89fca52c7b2dfbd2d603db73593
Aurus <55865051+AAurus@users.noreply.github.com>
2022-10-16 00:23:02 -0700

    minor checkstyle improvements

--- src/main/utility/ListOfHelper.java
+++ src/main/utility/ListOfHelper.java
@@ -9,6 +9,7 @@ import java.util.List;
 public class ListOfHelper {
     ///A utility class SOLELY CREATED to replicate the ListOfHelper.listOf(E... elements) method.
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<ScoreType> listOf(ScoreType... elements) {
         List<ScoreType> result = new ArrayList<>();
         for (ScoreType element : elements) {
@@ -17,6 +18,7 @@ public class ListOfHelper {
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<StatType> listOf(StatType... elements) {
         List<StatType> result = new ArrayList<>();
         for (StatType element : elements) {
@@ -25,65 +27,73 @@ public class ListOfHelper {
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<ProficiencyType> listOf(ProficiencyType... elements) {
         List<ProficiencyType> result = new ArrayList<>();
-        for (ProficiencyType e :  elements) {
+        for (ProficiencyType e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<ModifierType> listOf(ModifierType... elements) {
         List<ModifierType> result = new ArrayList<>();
-        for (ModifierType e :  elements) {
+        for (ModifierType e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<Feature> listOf(Feature... elements) {
         List<Feature> result = new ArrayList<>();
-        for (Feature e :  elements) {
+        for (Feature e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<Proficiency> listOf(Proficiency... elements) {
         List<Proficiency> result = new ArrayList<>();
-        for (Proficiency e :  elements) {
+        for (Proficiency e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<InventoryItem> listOf(InventoryItem... elements) {
         List<InventoryItem> result = new ArrayList<>();
-        for (InventoryItem e :  elements) {
+        for (InventoryItem e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<String> listOf(String... elements) {
         List<String> result = new ArrayList<>();
-        for (String e :  elements) {
+        for (String e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<CharClass> listOf(CharClass... elements) {
         List<CharClass> result = new ArrayList<>();
-        for (CharClass e :  elements) {
+        for (CharClass e : elements) {
             result.add(e);
         }
         return result;
     }
 
+    //EFFECTS: returns a List containing the elements passed in
     public static final List<CharRace> listOf(CharRace... elements) {
         List<CharRace> result = new ArrayList<>();
-        for (CharRace e :  elements) {
+        for (CharRace e : elements) {
             result.add(e);
         }
         return result;
